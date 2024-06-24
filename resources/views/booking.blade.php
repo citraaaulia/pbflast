b4320c63e9c663bff29ff78660b7d738f20d6f9e
2111521014 Iqbal Firmansyah <2111521014_iqbal@unand.ac.id>
2024-06-24 11:58:01 +0700

    update ibal

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -13,8 +13,8 @@
     </style>
 </head>
 <body class="bg-gray-100">
-    <div class="container mx-auto mt-10">
-        <div class="bg-white p-10 rounded-lg shadow-lg flex justify-between">
+    <div class="container mx-auto mt-10 max-w-4xl">
+        <div class="bg-white p-10 rounded-lg shadow-lg flex flex-col md:flex-row justify-between">
             <div>
                 <h2 class="text-2xl font-bold mb-4">Gedung Seminar I</h2>
                 <div class="mb-6">
@@ -22,7 +22,8 @@
                 </div>
                 <p class="text-green-500">• Sudah di-booking</p>
             </div>
-            <div class="ml-10">
+            
+            <div class="md:w-1/2 md:ml-10 mt-10 md:mt-0">
                 <h2 class="text-2xl font-bold mb-4">Isi Formulir</h2>
                 @if(session('success'))
                     <div class="bg-green-500 text-white p-4 mb-4">
